a726ff378deff8c7fe3d1a32ced64cef053e7d70
chvif <cherif.mehdi2005@gmail.com>
2026-08-05 10:40:49 +0100

    trying time in utc in production4

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -8,7 +8,8 @@ const db = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    dateStrings: true
 });
 
 db.connect((err) => {
