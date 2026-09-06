583849203cd24408abadd62ac50dd5d5354ec263
chvif <cherif.mehdi2005@gmail.com>
2026-09-06 09:51:44 +0100

    same same2

--- back-end/emailservice/emailService.js
+++ back-end/emailservice/emailService.js
@@ -6,26 +6,48 @@ const mailjet = new Mailjet({
 });
 
 async function sendEmailToDev(subject, message) {
-    await mailjet
-        .post("send", { version: "v3.1" })
-        .request({
-            Messages: [
-                {
-                    From: {
-                        Email: process.env.MAIL_FROM_EMAIL,
-                        Name: process.env.MAIL_FROM_NAME
-                    },
-                    To: [
-                        {
-                            Email: process.env.DEV_EMAIL,
-                            Name: process.env.DEV_NAME
-                        }
-                    ],
-                    Subject: subject,
-                    HTMLPart: message
-                }
-            ]
+    try {
+        console.log("mailjet: sending email to dev", { to: process.env.DEV_EMAIL, subject });
+
+        const res = await mailjet
+            .post("send", { version: "v3.1" })
+            .request({
+                Messages: [
+                    {
+                        From: {
+                            Email: process.env.MAIL_FROM_EMAIL,
+                            Name: process.env.MAIL_FROM_NAME
+                        },
+                        To: [
+                            {
+                                Email: process.env.DEV_EMAIL,
+                                Name: process.env.DEV_NAME
+                            }
+                        ],
+                        Subject: subject,
+                        HTMLPart: message
+                    }
+                ]
+            });
+
+        // mailjet responds with an object containing "body" and "status" sometimes
+        console.log("mailjet: send result", {
+            status: res && res.statusCode ? res.statusCode : res && res.body ? res.body.Status : undefined,
+            body: res && res.body ? res.body : res
         });
+
+        return true;
+    } catch (err) {
+        // Try to surface useful information from the error
+        console.error("mailjet: failed to send email", {
+            message: err && err.message,
+            code: err && err.code,
+            responseBody: err && err.response && err.response.body ? err.response.body : undefined,
+            stack: err && err.stack
+        });
+
+        return false;
+    }
 }
 
 module.exports = { sendEmailToDev };
